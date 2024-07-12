7aa1ef6c2deafa32e0c00a3304fee5a469eb50cd
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-07-12 04:06:22 +0300

    ip addresses fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['52.207.113.157', '34.203.201.71']
 
 
 def do_deploy(archive_path):
